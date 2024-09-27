fa14199a09768467e69b402cd9ab3e5840376568
RA9R <r@a9r.me>
2024-09-27 08:50:44 -0400

    Initial Commit

---
+++ TaxStudy/TaxStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxStudyApp.swift
+//  TaxStudy
+//
+//  Created by Rodney Aiglstorfer on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TaxStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
