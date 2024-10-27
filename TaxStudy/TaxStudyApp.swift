3ec568c9435016e42d779920a03becee4cf74102
RA9R <r@a9r.me>
2024-10-27 06:42:01 -0400

    added TaxFactsEditor as settings menu

--- TaxStudy/TaxStudyApp.swift
+++ TaxStudy/TaxStudyApp.swift
@@ -9,10 +9,24 @@ import SwiftUI
 
 @main
 struct TaxStudyApp: App {
+    @Environment(\.openWindow) var openWindow
+    @State var allFacts: [TaxFacts] = [DefaultTaxFacts2024]
     
     var body: some Scene {
         DocumentGroup(newDocument: TaxProjectDocument()) { file in
             ProjectView(file.$document)
+        }
+        .commands {
+            CommandGroup(after: CommandGroupPlacement.appInfo) {
+                Button("Settings...") {
+                    openWindow(id: "TaxFactsEditor")
+                }
+                .keyboardShortcut(",", modifiers: .command)
+            }
+        }
+        
+        WindowGroup("Tax Facts", id: "TaxFactsEditor") {
+            SettingsView(facts: $allFacts)
         }
     }
 
